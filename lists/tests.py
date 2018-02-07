904ea20037294b770fb3f7c6f69bd713cce97f62
sundhhy <sundhhy>
2018-02-07 08:29:51 +0800

    为item建立发相关的模型

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,9 @@ from django.http import HttpRequest
 #from django.core.urlresolvers import  resolve
 from django.urls import reverse
 from django.template.loader import render_to_string
+
+from .models import Item
+
 # Create your tests here.
 class Homepage(TestCase):
     def test_root_url_resolves_to_home_page(self):
@@ -17,7 +20,10 @@ class Homepage(TestCase):
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
+        #print(html)
+
         expected_html = render_to_string('home.html')
+        #print(expected_html)
         self.assertEqual(html.strip(), expected_html)
 
     def test_use_home_templates(self):
@@ -37,3 +43,24 @@ class Homepage(TestCase):
             {'new_item_text': 'A new list item'}
         )
         self.assertEqual(response.content.decode(), expected_html)
+
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retriving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Iten the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Iten the second')
