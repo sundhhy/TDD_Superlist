90bd2607e7ee438673a0e7d3554103a760c77f39
sundhhy <sundhhy>
2018-02-09 13:56:42 +0800

    学习到5.8章

--- lists/views.py
+++ lists/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
+from .models import Item
 # Create your views here.
 #def home_page(request):
 #    return HttpResponse('<html><title>To-Do lists</title></html>')
 
 def home_page(request):
-#    if request.method == 'POST':
-#        return HttpResponse(request.POST['item_text'])
-    return render(request, 'home.html', {
-        'new_item_text':request.POST.get('item_text', ''),
-    })
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+
+
+    return render(request, 'home.html')
