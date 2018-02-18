3ce2ceac42a7e3c9f5fb4a7d380f767dfd81a951
sundhhy <418755805@qq.com>
2018-02-18 20:56:15 +0800

    调整模型，让待办事项和不同的清单关联起来

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
+class List(models.Model):
+    pass
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, null=True, on_delete=models.SET_NULL)
+
 
 # Create your models here.
--- lists/tests.py
+++ lists/tests.py
@@ -5,7 +5,7 @@ from django.http import HttpRequest
 from django.urls import reverse
 from django.template.loader import render_to_string
 
-from .models import Item
+from .models import Item, List
 
 # Create your tests here.
 class Homepage(TestCase):
@@ -30,15 +30,23 @@ class Homepage(TestCase):
         response = self.client.get('/')
         #self.assertEqual(response.content.decode('utf8'),render_to_string('home.html'))
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retriving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Iten the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -47,7 +55,9 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
 
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Iten the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -56,8 +66,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
