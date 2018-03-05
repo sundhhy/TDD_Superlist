a57432ae90a2d0138564dc6ff750147660686493
sundhhy <418755805@qq.com>
2018-03-05 15:31:31 +0800

    把数据库移到主程序源码树之外

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -14,7 +14,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         for arg in sys.argv:
-            print(arg)
             if 'liveserver' in arg:
                 cls.server_url = 'http://' + arg.split('=')[1]
 
--- superlists/settings.py
+++ superlists/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+#BASE_DIR = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
     }
 }
 
