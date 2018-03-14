09ff06125572c10c7d82f80b916c46cd726460f4
sundhhy <418755805@qq.com>
2018-03-14 09:44:30 +0800

    把单元测试拆分到2个文件中

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -9,55 +9,9 @@ from lists.models import Item, List
 
 # Create your tests here.
 class Homepage(TestCase):
-    def test_root_url_resolves_to_home_page(self):
-        found = reverse('home')
-        #self.assertEqual(found.func, home_page)
-        #django 2.01已经不支持resolve了
-        print('test resolves')
-        self.assertEqual(found, '/')
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-
-        expected_html = render_to_string('home.html')
-        #django2.0之后render_to_string无法处理csrf_token，所以这个测试就无法通过了
-        #self.assertEqual(response.content.decode(), expected_html)
-
-
     def test_use_home_templates(self):
         response = self.client.get('/')
         #self.assertEqual(response.content.decode('utf8'),render_to_string('home.html'))
-
-class ListAndItemModelTest(TestCase):
-    def test_saving_and_retriving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Iten the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Iten the second')
-        self.assertEqual(second_saved_item.list, list_)
-
 
 class ListViewTest(TestCase):
     def test_uses_lists_template(self):
