d4d1279246a5233c0c1692201da0d9d0d5244a1b
sundhhy <418755805@qq.com>
2018-03-31 13:31:03 +0800

    为lits的item增加表单

--- lists/views.py
+++ lists/views.py
@@ -28,16 +28,7 @@ def view_list(request, list_id):
             error = "You can't have an empty list item"
     return render(request, 'list.html', {'list': list_, 'error': error})
 
-'''
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    error = None
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'], list=list_)
-        return redirect('/lists/{}/'.format(list_.id))
 
-    return render(request, 'list.html', {'list': list_, 'error': error})
-'''
 
 
 
@@ -51,13 +42,8 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error":error})
-    return redirect('/lists/%d/'%(list_.id,))
-'''
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/{}/' .format (list_.id))
-'''
+    return redirect('view_list', list_.id)
+
 
 
 
